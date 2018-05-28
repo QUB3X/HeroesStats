2a9b35f9f1cefafa405f023ef76db166e8f33798
Andrea Franchini <f.andrea1602@gmail.com>
2018-05-28 14:06:25 +0200

    Initial Commit

---
+++ Heroes Stats/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Heroes Stats
+//
+//  Created by Andrea Franchini on 28/05/2018.
+//  Copyright © 2018 Andrea Franchini. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
