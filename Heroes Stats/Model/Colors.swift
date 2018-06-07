2e3ed7902940857af557e94a342476fb44c01451
Andrea Franchini <f.andrea1602@gmail.com>
2018-06-07 10:20:35 +0200

    updated theme and layout

---
+++ Heroes Stats/Model/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  Heroes Stats
+//
+//  Created by Andrea Franchini on 07/06/2018.
+//  Copyright © 2018 Andrea Franchini. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    struct Accent {
+        struct Purple {
+            static let superlight = UIColor(red: 208, green: 163, blue: 255, alpha: 255)
+            static let light = UIColor(red: 163, green: 96, blue: 251, alpha: 255)
+            static let normal = UIColor(red: 137, green: 0, blue: 255, alpha: 255)
+            static let dark = UIColor(red: 113, green: 0, blue: 210, alpha: 255)
+            static let superdark = UIColor(red: 62, green: 0, blue: 116, alpha: 255)
+        }
+    }
+}
